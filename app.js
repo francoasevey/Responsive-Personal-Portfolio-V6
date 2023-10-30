153932927011d89f7f2e17b9a0949c66c4583f77
francoromeo <francoromeo5012@gmail.com>
2023-10-30 00:01:47 -0300

    fifnish

--- app.js
+++ app.js
@@ -49,3 +49,33 @@ const testimonialSlide = new Swiper(".testimonial_wrapper", {
         },
     },
 });
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 40 ){
+        header.classList.add("header-scroll");
+    } else {
+        header.classList.remove("header-scroll");
+    }
+})
+
+const sr = ScrollReveal({
+    duration: 2000,
+    distance: "100px",
+    delay: 400,
+    reset: true,
+});
+
+sr.reveal(".hero_content, .about_content");
+sr.reveal("hero_img", {origin: "top"});
+
+sr.reveal(
+    ".hero_info-wrapper, .skills_title, .skills_content, .qualification_name, .qualification_item, .service_card, .project_content, .testimonial_wrapper, .footer_content",
+    {
+        delay: 500,
+        interval: 100,
+    }
+);
+sr.reveal("qualification_footer-text, .contact_content", {
+    origin: "left",
+})
+sr.reveal(".qualification_footer .btn, .contact_btn", {origin: "right"});
